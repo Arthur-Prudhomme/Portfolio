f325540aa09cbd34076cb8f9c5a2d22e250b7657
Arthur Prud'homme <86523707+Arthur-Prudhomme@users.noreply.github.com>
2022-10-26 14:28:51 +0200

    :sparkles: dark/light mode switch

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 function openPage(evt, pageName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(pageName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function toggleLightMode() {
+  var nav_e = document.getElementById("nav");
+  var global = document.body;
+  var contact_e = document.getElementById("sc-contacts");
+  nav_e.classList.toggle("light-mode-second");
+  global.classList.toggle("light-mode-body");
+  contact_e.classList.toggle("light-mode-second");
+}
