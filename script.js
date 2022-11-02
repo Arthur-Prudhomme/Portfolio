f138f287b85c627f6b46bf32d3638dc2964678e9
arthur <arthurprudhommemail06@gmail.com>
2022-11-02 08:59:16 +0100

    :bug: fix nav burger

--- script.js
+++ script.js
@@ -25,10 +25,16 @@ function toggleLightMode() {
 }
 
 var burgerMenu = document.getElementById('burger-menu');
-
 var overlay = document.getElementById('menu');
+var li_nav = document.getElementById('c-nav');
 
 burgerMenu.addEventListener('click', function() {
   this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+
+overlay.addEventListener('click', function() {
+  burgerMenu.classList.toggle("close");
   overlay.classList.toggle("overlay");
 });
