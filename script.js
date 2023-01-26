f7ebaeeea76b9ae1880a0bd2abd36532cf8ce130
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2023-01-26 23:09:46 +0100

    :lipstick: add onscroll animation

--- script.js
+++ script.js
@@ -28,3 +28,17 @@ window.onmousemove = e => {
 
     animeMouse(e, interacting)
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
